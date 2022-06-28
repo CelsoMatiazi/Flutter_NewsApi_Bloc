0f3c2a727eb8b096e60ad2a8b9403d5403a9bd27
CelsoMatiazi <celsomatiazi@gmail.com>
2022-06-28 11:07:24 -0300

    extracting codes to files

--- lib/Screens/home_screen/components/search_bar.dart
+++ lib/Screens/home_screen/components/search_bar.dart
@@ -1,11 +1,11 @@
-import 'dart:math';
-import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_news_flutter/Screens/home_screen/bloc_controller/home_screen_bloc.dart';
-import 'package:google_news_flutter/Screens/home_screen/bloc_controller/home_screen_event.dart';
 import 'package:google_news_flutter/Screens/home_screen/bloc_controller/home_screen_state.dart';
-import 'package:lottie/lottie.dart';
+import 'package:google_news_flutter/Screens/home_screen/components/search_field_widget.dart';
+import 'countries_flags_widget.dart';
+
 
 class SearchBar extends StatefulWidget {
   const SearchBar({
@@ -44,141 +44,10 @@ class _SearchBarState extends State<SearchBar> with SingleTickerProviderStateMix
         return Stack(
           alignment: Alignment.topCenter,
           children: [
-            AnimatedContainer(
-              duration: const Duration(milliseconds: 600),
-              height: height == 0.0 ? 80 : 0,
-              width: double.maxFinite,
-              color: Colors.black,
-              child: ListView.builder(
-                  itemCount: country.length,
-                  scrollDirection: Axis.horizontal,
-                  physics: const BouncingScrollPhysics(),
-                  itemBuilder: (context, index){
-                    return Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        BlocSelector<HomeScreenBloc, HomeScreenState, String>(
-                          selector: (state){
-                            return state.country ?? "";
-                          },
-                          builder: (_, selectedCountry){
-                            if(country[index] == selectedCountry){
-                              return Stack(
-                                alignment: Alignment.center,
-                                children: [
-                                  Lottie.asset("assets/animations/moving_circle.json", width: 78),
-                                  AnimatedBuilder(
-                                    animation: _controller,
-                                    builder: (_,child) {
-                                      return Transform.rotate(
-                                          angle: _controller.value * 2 * pi,
-                                          child: child,
-                                      );
-                                    },
-                                    child: Container(
-                                      height: 48,
-                                      width: 48,
-                                      decoration: BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          gradient: LinearGradient(
-                                              begin:Alignment.centerLeft ,
-                                              end: Alignment.bottomRight,
-                                              colors: [Colors.pink, Colors.deepPurple[700]!,])
-                                          ),
-                                    ),
-                                  ),
-                                ],
-                              );
-                            }else{
-                              return Container(
-                                margin: EdgeInsets.symmetric(horizontal: 16),
-                                height: 46,
-                                width: 46,
-                                decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    gradient: LinearGradient(
-                                        begin:Alignment.centerLeft ,
-                                        end: Alignment.bottomRight,
-                                        colors: [Colors.deepOrange[800]!, Colors.orange[300]!,])
-                                ),
 
-                              );
-                            }
-                          }
-                        ),
+            CountriesFlagsWidget(country: country, controller: _controller, height: height,),
 
-                        GestureDetector(
-                          child: Container(
-                            height: 40,
-                            width: 40,
-                            decoration:  BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.transparent,
-                              border: Border.all(color: Colors.black),
-                              image: DecorationImage(
-                                  image: NetworkImage("https://www.bandeirasnacionais.com/data/flags/normal/${country[index]}.png"), fit: BoxFit.cover,
-                              )
-                            ),
-
-                          ),
-                          onTap: (){
-                            context.read<HomeScreenBloc>().add(HomeScreenEventFindAll(country: country[index]));
-                          },
-                        ),
-                      ],
-                    );
-                  }
-              ),
-            ),
-
-
-            AnimatedContainer(
-              duration: const Duration(milliseconds: 500),
-              padding: const EdgeInsets.only(right: 15, left: 15, bottom: 0, top: 1),
-              height: height,
-              width: double.maxFinite,
-              decoration: const BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(15),
-                  bottomRight: Radius.circular(15),
-                ),
-              ),
-              child: TextField(
-                controller: _textCtrl,
-                style: const TextStyle(fontSize: 18),
-                decoration: InputDecoration(
-                    filled: true,
-                    hintText: "Search",
-                    hintStyle: const TextStyle(fontSize: 16),
-                    contentPadding: const EdgeInsets.only(left: 15),
-                    fillColor: Colors.white,
-                    hoverColor: Colors.white,
-                    suffixIcon: height == 0.0
-                    ? null
-                    : IconButton(
-                        icon: const Icon(
-                            Icons.search,
-                            color: Colors.black45),
-                        onPressed: () {
-                          if(_textCtrl.text.isNotEmpty){
-                            context.read<HomeScreenBloc>().add(HomeScreenEventFindByWord(word: _textCtrl.text));
-                            _textCtrl.clear();
-                            FocusScope.of(context).unfocus();
-                          }
-
-                        },
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.white54),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    )
-                ),
-              ),
-            ),
+            SearchFieldWidget(textCtrl: _textCtrl, height: height,),
 
           ],
         );
@@ -186,3 +55,4 @@ class _SearchBarState extends State<SearchBar> with SingleTickerProviderStateMix
     );
   }
 }
+
